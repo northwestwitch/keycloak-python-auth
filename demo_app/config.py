5e6c4521901eb7b2eb6eb24110142405a8799c47
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2025-03-21 09:51:24 +0100

    revert config

--- demo_app/config.py
+++ demo_app/config.py
@@ -3,7 +3,7 @@ class Config:
 
     FLASK_SECRET_KEY = "your_flask_secret_key"
     KEYCLOAK_CLIENT_ID = "scout-client"
-    KEYCLOAK_CLIENT_SECRET = "JV5DRR3YvxW5JEsUOD3UXtbhqvaxKHdd"
+    KEYCLOAK_CLIENT_SECRET = "3VFpvHLXUyiNMcCxGOKe5vld378yZLqd"
     REALM = "CG"
     KEYCLOAK_SERVER_METADATA_URL = (
         f"http://localhost:8080/realms/{REALM}/.well-known/openid-configuration"
